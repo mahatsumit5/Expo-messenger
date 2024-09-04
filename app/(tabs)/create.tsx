fb68cbe980eba9e1a0cab79af5fb9b50c877d11c
mahatsumit5 <mahatsumit5@gmail.com>
2024-09-05 00:58:34 +1000

    "Added KeyboardAvoidingView and TouchableWithoutFeedback components to several screens to handle keyboard input and scrolling."

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,4 +1,14 @@
-import { View, Text, ScrollView, Image, FlatList, Alert } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  FlatList,
+  Alert,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import Inputfield from "@/components/Inputfield";
 import CustomButton from "@/components/CustomButton";
@@ -72,57 +82,65 @@ const Profile = () => {
     }
   }
   return !isError ? (
-    <ScrollView className="px-2 relative bg-background">
-      <View className="my-10">
-        <View className="mb-10">
-          <Inputfield
-            title="Title"
-            onChange={(e) => {
-              setForm({ ...form, title: e });
-            }}
-            placeholder="Enter your Title"
-            keyboardType="default"
-            value={form.title}
-          />
-          <Inputfield
-            title="Description"
-            onChange={(e) => {
-              setForm({ ...form, content: e });
-            }}
-            placeholder="Message"
-            keyboardType="default"
-            value={form.content}
-            inputHeight="h-[150px]"
-          />
-        </View>
-        <View className="absolute top-8 right-0 flex flex-row">
-          <TouchableIcon
-            onPress={openCamera}
-            iconClassName="mr-5 h-8 w-8"
-            icon={Icons.camera}
-          />
-          <TouchableIcon
-            onPress={pickImage}
-            icon={Icons.img}
-            iconClassName="h-8 w-8"
-          />
-        </View>
+    <KeyboardAvoidingView behavior="padding" className="">
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View className="px-2 relative bg-background">
+          <View className="my-10">
+            <View className="mb-10">
+              <Inputfield
+                title="Title"
+                onChange={(e) => {
+                  setForm({ ...form, title: e });
+                }}
+                placeholder="Enter your Title"
+                keyboardType="default"
+                value={form.title}
+              />
+              <Inputfield
+                title="Description"
+                onChange={(e) => {
+                  setForm({ ...form, content: e });
+                }}
+                placeholder="Message"
+                keyboardType="default"
+                value={form.content}
+                inputHeight="h-[150px]"
+              />
+            </View>
+            <View className="absolute top-8 right-0 flex flex-row">
+              <TouchableIcon
+                onPress={openCamera}
+                iconClassName="mr-5 h-8 w-8"
+                icon={Icons.camera}
+              />
+              <TouchableIcon
+                onPress={pickImage}
+                icon={Icons.img}
+                iconClassName="h-8 w-8"
+              />
+            </View>
 
-        <View className="">
-          <ImageSlider images={form.images} onPress={() => {}} />
-        </View>
+            <View className="">
+              <ImageSlider images={form.images} onPress={() => {}} />
+            </View>
 
-        <View>
-          <CustomButton
-            title="Create a post"
-            isLoading={isLoading}
-            onPress={handleSubmit}
-            disabled={!form.title || !form.content}
-          />
+            <View>
+              <CustomButton
+                title="Create a post"
+                isLoading={isLoading}
+                onPress={handleSubmit}
+                disabled={!form.title || !form.content}
+              />
+            </View>
+            <CustomStatusBar
+              backgroundColor="white"
+              style="dark"
+              hidden={false}
+            />
+          </View>
         </View>
-        <CustomStatusBar backgroundColor="white" style="dark" hidden={false} />
-      </View>
-    </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   ) : (
     <Text>Error Occured</Text>
   );
--- app/(tabs)/message/[room].tsx
+++ app/(tabs)/message/[room].tsx
@@ -1,4 +1,14 @@
-import { View, Text, SafeAreaView, TextInput } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+  FlatList,
+} from "react-native";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
 import { useAppSelector } from "@/hooks/hooks";
@@ -9,26 +19,32 @@ const Message = () => {
   const { roomId } = useLocalSearchParams();
   const { room } = useAppSelector((store) => store.room);
   return room?.id ? (
-    <View className="h-full justify-between ">
-      <View className="p-2">
-        <Text>Message Display</Text>
-      </View>
-      <View className="p-2  flex-row ">
-        <TextInput
-          className="flex-1  h-10 rounded-md p-2 bg-slate-300"
-          placeholder="enter you message"
-          placeholderTextColor={"gray"}
-        />
-        <View className="ml-2">
-          <SmallIconButton
-            icon={Icons.send}
-            title="Send"
-            onPress={() => {}}
-            variant="bg-primary"
-          />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View className=" flex-col h-full justify-between ">
+          <View className="p-2 flex-1 bg-red">
+            <FlatList data={[]} renderItem={({ item }) => <View></View>} />
+          </View>
+          <View className="p-2  flex-row ">
+            <TextInput
+              className="flex-1  h-10 rounded-md p-2 bg-slate-300"
+              placeholder="enter you message"
+              placeholderTextColor={"gray"}
+            />
+            <View className="ml-2">
+              <SmallIconButton
+                icon={Icons.send}
+                title="Send"
+                onPress={() => {}}
+                variant="bg-primary"
+              />
+            </View>
+          </View>
         </View>
-      </View>
-    </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   ) : null;
 };
 
